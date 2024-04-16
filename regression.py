311331f75fa7054380062cd94c71513dce0df57e
devude7 <dawidmrowczynski7@gmail.com>
2024-04-16 22:02:00 +0200

    training (need fixed)

--- regression.py
+++ regression.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from helpers import predict, sigmoid_derivative
+from helpers import predict, sigmoid_derivative, sigmoid_activation
 
 data = pd.read_csv("hou_all.csv")
 data.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV', 'BIAS_COL']
 
 # hyperparameters
-epochs = 2
+epochs = 20
 lr = 0.01
 
 X = data.drop('MEDV', axis=1)
@@ -18,10 +18,21 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3 , random
 W = np.random.randn(X.shape[1])
 
 losses = []
-for epoch in range(epochs):
-    prediction = predict(X_train, W)
-    errors = y_train - prediction
-    losses.append(errors.sum())
-    W += -lr * X_train.T.dot(errors)
+for epoch in np.arange(0, epochs):
 
-    print(losses)
+    #activation
+    preds = sigmoid_activation(X_train.dot(W))
+    errors = preds - y_train
+    
+    loss = np.sum(errors ** 2)
+    losses.append(loss)
+
+    # gradient descent
+    d = errors * sigmoid_derivative(preds)
+    gradient = X_train.T.dot(d)
+    
+    # update weights
+    W += -lr * gradient
+    
+    if epoch == 0 or (epoch + 1) % 1 == 0:
+        print("[INFO] epoch={}, loss={:.7f}".format(int(epoch + 1), loss))
