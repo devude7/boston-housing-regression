1aef05baf3b35145ffed0a649d350173af04838d
devude7 <dawidmrowczynski7@gmail.com>
2024-04-16 14:57:41 +0200

    helpers and fix

--- regression.py
+++ regression.py
@@ -1,16 +1,27 @@
 import numpy as np
 import pandas as pd
-import sklearn
 from sklearn.model_selection import train_test_split
+from helpers import predict, sigmoid_derivative
 
 data = pd.read_csv("hou_all.csv")
 data.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV', 'BIAS_COL']
 
-data = data.drop('BIAS_COL', axis = 1)
+# hyperparameters
+epochs = 2
+lr = 0.01
 
-X = data.iloc[:, :-1]
-y = data.iloc[:, -1]
+X = data.drop('MEDV', axis=1)
+y = data.iloc[:, 13]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=32)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3 , random_state=32)
 
+W = np.random.randn(X.shape[1])
 
+losses = []
+for epoch in range(epochs):
+    prediction = predict(X_train, W)
+    errors = y_train - prediction
+    losses.append(errors.sum())
+    W += -lr * X_train.T.dot(errors)
+
+    print(losses)
