b93ec62b7516f52ca1527d1d3a90ffde09c0704e
devude7 <dawidmrowczynski7@gmail.com>
2024-04-16 14:21:04 +0200

    data split

--- regression.py
+++ regression.py
@@ -1,5 +1,16 @@
 import numpy as np
 import pandas as pd
+import sklearn
+from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("hou_all.csv")
-df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV', 'BIAS_COL']
+data = pd.read_csv("hou_all.csv")
+data.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV', 'BIAS_COL']
+
+data = data.drop('BIAS_COL', axis = 1)
+
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=32)
+
+
