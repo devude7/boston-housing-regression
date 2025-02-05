27ee86285ee94e446b50475db94cef8cdced1e8d
devude7 <dawidmrowczynski7@gmail.com>
2025-02-05 18:29:49 +0100

    Linear regression

--- regression.py
+++ regression.py
@@ -1,30 +1,36 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from backpropagation import NeuralNetwork
+from neural_network import NeuralNetwork
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("hou_all.csv")
 data.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV', 'BIAS_COL']
 
-# hyperparameters
-epochs = 25
-lr = 0.1
+# Hyperparameters
+epochs = 200
+lr = 0.01
 
 data = data.drop('BIAS_COL', axis=1)
 X = data.drop('MEDV', axis=1)
 y = data.iloc[:, 13]
 
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+y = np.array(y)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3 , random_state=32)
 
-# train the network
-print("[INFO] training network...")
-nn = NeuralNetwork([X.shape[1], 2, 2, 1])
-print("[INFO] {}".format(nn))
-nn.fit(X_train, y_train, epochs=1000)
+# Train the network
+nn = NeuralNetwork([X.shape[1], 64, 32, 16, 1], epochs, "sigmoid", lr)
+nn.fit(X_train, y_train)
 
-# evaluate the network
+print("[INFO] training network...")
+
+# Evaluate the network
 print("[INFO] evaluating network...")
 predictions = nn.predict(X_test)
-error = predictions - np.atleast_2d(y_test)
-print(f'Global error: {np.sum(error)}')
 
+mse = np.mean((y_test - predictions) ** 2)
+print(f"Mean Squared Error: {mse:.2f}")
