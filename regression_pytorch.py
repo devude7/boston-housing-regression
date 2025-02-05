4e436209b45f8d0c4f6a4f9439e0ac961bc8797a
devude7 <dawidmrowczynski7@gmail.com>
2025-02-05 22:33:15 +0100

    result plots

--- regression_pytorch.py
+++ regression_pytorch.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("hou_all.csv")
 data.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV', 'BIAS_COL']
@@ -67,3 +68,36 @@ with torch.no_grad():
     test_loss = criterion(y_pred, y_test)
     print(f'Test Loss: {test_loss.item():.4f}')
 
+# Denorm
+predictions = scaler_y.inverse_transform(y_pred.cpu().numpy())
+y_test = scaler_y.inverse_transform(y_test.cpu().numpy())
+
+# Regresion graph
+plt.scatter(y_test, predictions)
+plt.title("Regression - Boston Housing")
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red')
+plt.show()
+
+# Plot real and predicted values
+plt.figure(figsize=(10, 5))
+plt.scatter(range(len(y_test)), y_test, color='blue', label='Real values')
+plt.scatter(range(len(predictions)), predictions, color='green', label='Predicted values', marker='x')
+plt.title("Real vs. Predicted")
+plt.legend()
+plt.show()
+
+# Change to 1-dim arrays for plotting
+y_test = np.array(y_test).flatten()
+predictions = np.array(predictions).flatten()
+
+# Plot lines to link predicted to real values
+plt.figure(figsize=(10, 5))
+plt.scatter(range(len(y_test)), y_test, color='blue', label='Real values')
+plt.scatter(range(len(predictions)), predictions, color='green', label='Predicted values', marker='x')
+
+for i, (yt, yp) in enumerate(zip(y_test, predictions)):
+    plt.plot([i, i], [yt, yp], 'gray', linestyle='dotted', alpha=0.6)
+
+plt.title("Real vs. Predicted")
+plt.legend()
+plt.show()
